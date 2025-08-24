5312cd46cdabfa64e5594e09763c2dbb9dac2982
bilalfakhouri <actorjad@gmail.com>
2025-08-24 08:49:15 +0300

    M50: enable CONFIG_MMU_REMAP Works now for my M50

--- platform/M50.110/features.h
+++ platform/M50.110/features.h
@@ -24,9 +24,8 @@
 #define FEATURE_CROPMARKS
 
 // Enable remapping ROM pages to RAM
-// Doesn't work yet
-//#define CONFIG_SGI_HANDLERS
-//#define CONFIG_MMU_REMAP
+#define CONFIG_SGI_HANDLERS
+#define CONFIG_MMU_REMAP
 
 //~kitor: add bootdisk menu for easier testing on foreign camera :)
 #define FEATURE_BOOTFLAG_MENU
